9a822785ecfbe3e62576fb646faa3beb0e2baf1e
adiluexe <adiluexe@gmail.com>
2024-08-28 15:14:39 +0800

    Feat: Add calculator class
    - Update display when clicking the number buttons

---
+++ script.js
@@ -0,0 +1,50 @@
+class Calculator {
+  constructor(previousOperandElement, currentOperandElement) {
+    this.previousOperandElement = previousOperandElement;
+    this.currentOperandElement = currentOperandElement;
+    this.clear();
+  }
+
+  clear() {
+    this.currentOperand = "";
+    this.previousOperand = "";
+    this.operation = undefined;
+  }
+
+  delete() {}
+
+  appendNumber(number) {
+    if (number === "." && this.currentOperand.includes(".")) return;
+    this.currentOperand = this.currentOperand.toString() + number.toString();
+  }
+
+  chooseOperation(operation) {}
+
+  compute() {}
+
+  updateDisplay() {
+    this.currentOperandElement.innerText = this.currentOperand;
+  }
+}
+
+const numberButtons = document.querySelectorAll("[data-number]");
+const operationButtons = document.querySelectorAll("[data-operation]");
+const equalsButton = document.querySelector("[data-equals]");
+const allClearButton = document.querySelector("[data-all-clear]");
+const deleteButton = document.querySelector("[data-delete]");
+const previousOperandElement = document.querySelector(
+  "[data-previous-operand]"
+);
+const currentOperandElement = document.querySelector("[data-current-operand]");
+
+const calculator = new Calculator(
+  previousOperandElement,
+  currentOperandElement
+);
+
+numberButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    calculator.appendNumber(button.innerText);
+    calculator.updateDisplay();
+  });
+});
