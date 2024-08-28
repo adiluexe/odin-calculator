fe98f0160412817bd0c974de6f346771523ecc73
adiluexe <adiluexe@gmail.com>
2024-08-28 15:22:12 +0800

    Feat: Append previous operand to top of current operand when operation is performed

--- script.js
+++ script.js
@@ -18,12 +18,21 @@ class Calculator {
     this.currentOperand = this.currentOperand.toString() + number.toString();
   }
 
-  chooseOperation(operation) {}
+  chooseOperation(operation) {
+    if (this.currentOperand === "") return;
+    if (this.previousOperand !== "") {
+      this.compute();
+    }
+    this.operation = operation;
+    this.previousOperand = this.currentOperand;
+    this.currentOperand = "";
+  }
 
   compute() {}
 
   updateDisplay() {
     this.currentOperandElement.innerText = this.currentOperand;
+    this.previousOperandElement.innerText = this.previousOperand;
   }
 }
 
@@ -45,6 +54,13 @@ const calculator = new Calculator(
 numberButtons.forEach((button) => {
   button.addEventListener("click", () => {
     calculator.appendNumber(button.innerText);
+    calculator.updateDisplay();
+  });
+});
+
+operationButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    calculator.chooseOperation(button.innerText);
     calculator.updateDisplay();
   });
 });
